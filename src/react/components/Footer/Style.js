19bf09424a3d370efb894b6332b966c1e2ccfde9
rawle51 <costa08@mail.ru>
2019-07-16 17:50:07 +0300

    refactor footer

---
+++ src/react/components/Footer/Style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.footer`
+  display: flex;
+  justify-content: center;
+  margin: 20px auto 0;
+`;
+
+export const Inner = styled.div`
+  display: flex;
+  align-items: center;
+  height: 50px;
+  line-height: 50px;
+`;
+
+export const Text = styled.span`
+  margin-right: 10px;
+`;
+
+export const Link = styled.a`
+  width: 50px;
+  height: 45px;
+`;
